12ed907a2e38ec56e582a8a69caf340ddec2c812
DanielRasho <ray22933@uvg.edu.gt>
2025-01-19 21:02:29 -0600

    batman

---
+++ cmd/function2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"hello-world/internal"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var greeting string
+	sourceIP := request.RequestContext.Identity.SourceIP
+
+	if sourceIP == "" {
+		greeting = "Bye World!\n"
+	} else {
+		greeting = fmt.Sprintf("Bye, %s!\n", sourceIP)
+	}
+	internal.Hello()
+
+	return events.APIGatewayProxyResponse{
+		Body:       greeting,
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
